f44b5656ac9a1e2d64ececb4c0bbecf33ea3e2c5
Mike Datsko <mike@datsko.it>
2017-06-25 14:51:36 +0300

    hidden facebook sharing

--- src/app/components/social-share/social-share.js
+++ src/app/components/social-share/social-share.js
@@ -12,13 +12,6 @@ export default class SocialShare extends Component {
     return (
       <div className="social-sharing">
         <a
-          href="javascript:void(0)"
-          className="button"
-        >
-          <span className="icon-facebook"></span>
-        </a>
-
-        <a
           href="https://twitter.com/intent/tweet?hashtags=reactriot&original_referer=https%3A//meaningoflife.herokuapp.com&ref_src=twsrc%5Etfw&related=twitterapi%2Ctwitter&text=Meaning of Life&tw_p=tweetbutton&url=https%3A//meaningoflife.herokuapp.com&via=mykhailodatsko"
           className="button"
           target="_blank"
@@ -26,7 +19,14 @@ export default class SocialShare extends Component {
           <span className="icon-twitter"></span>
         </a>
 
-        <iframe
+        {/*<a
+          href="javascript:void(0)"
+          className="button"
+        >
+          <span className="icon-facebook"></span>
+        </a>*/}
+
+        {/*<iframe
           id="social-sharing-fb-iframe"
           src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fmeaningoflife.herokuapp.com%2Findex.html&layout=button&size=large&mobile_iframe=true&appId=143455512882934&width=115&height=28"
           width="30"
@@ -34,7 +34,7 @@ export default class SocialShare extends Component {
           scrolling="no"
           allowTransparency="true"
         >
-        </iframe>
+        </iframe>*/}
       </div>
     )
   }
